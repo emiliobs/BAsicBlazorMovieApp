b8046e47dead64405fe594d5d3a156fca102af4d
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-07-25 14:52:34 -0500

    Add project files.

---
+++ BAsicBlazorMovieApp/Models/MovieModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BAsicBlazorMovieApp.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string ImagenUrl { get; set; } = null!;
+
+        public string Review { get; } = null!;
+    }
+}
